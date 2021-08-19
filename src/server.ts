646f6a1c18c7edbd5107a941ad27410926bad240
Davi Augusto <daviaugusto@MacBook-Pro-de-Davi.local>
2021-08-18 22:43:51 -0300

    fix: database create

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-/**
- *  GET    => Buscar uma Informação
- *  POST   => Inserir (Criar) uma Informação
- *  PUT    => Alterar uma Informação
- *  DELITE => Remover um dado
- *  PATCH  => Alterar uma Informação específica
- *
- *  Tipos de Parametros
- *
- *  Routes Params => http://localhost:3000/produtos/78347583448345
- *  Query Params  => http://localhost:3000/poducts?name=teclado&description=tecladobom&
- *  Query Params  => {
- *    "name": "teclado",
- *    "description": "teclado bom"
- *  }
- *  **/
-
-app.get("/test", (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
-  return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Ola test");
-});
 
 app.listen(3000, () => console.log("Server is Running"));
